bd203ee17d7c3df33a6218d9d4ab5ff12ac4aedc
rrauf8178 <rrauf8178@gmail.com>
2023-10-13 19:59:50 +0400

    feat/post: add post right and postcenter divided and styled

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -3,13 +3,13 @@ import { MoreVert } from "@material-ui/icons";
 import { Users } from "../../dummyData";
 import { useState } from "react";
 
-export default function Post({post}) {
+export default function Post({ post }) {
   return (
     <div className="post">
-        <div className="postWrapper">
-            <div className="postTop">
-                <div className="postTopLeft">
-                <img
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
               className="postProfileImg"
               src={Users.filter((u) => u.id === post?.userId)[0].profilePicture}
               alt=""
@@ -18,9 +18,16 @@ export default function Post({post}) {
               {Users.filter((u) => u.id === post?.userId)[0].username}
             </span>
             <span className="postDate">{post.date}</span>
-                </div>
-            </div>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
         </div>
+        <div className="postCenter">
+          <span className="postText">{post?.desc}</span>
+          <img className="postImg" src={post.photo} alt="" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
