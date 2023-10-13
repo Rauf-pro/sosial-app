956ce9c7c19cf312f84e45bfd6e15c314ece2ca1
rrauf8178 <rrauf8178@gmail.com>
2023-10-13 18:58:17 +0400

    feat/sidebar: add show more button and closefriend list

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -57,6 +57,11 @@ export default function Sidebar() {
         </ul>
         <button className="sidebarButton">Show More</button>
         <hr className="sidebarHr" />
+        <ul className="sidebarFriendList">
+          {Users.map((u) => (
+            <CloseFriend key={u.id} user={u} />
+          ))}
+        </ul>
       </div>
     </div>
   );
