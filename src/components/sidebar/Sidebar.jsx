16f30084142fa51c5bfead2333c2bde2d940ec7a
rrauf8178 <rrauf8178@gmail.com>
2023-10-13 18:33:27 +0400

    feat/sidebar: import some icons from material-ui/icons

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,23 @@
-import './sidebar.css'
+import './sidebar.css';
+import {
+    RssFeed,
+    Chat,
+    PlayCircleFilledOutlined,
+    Group,
+    Bookmark,
+    HelpOutline,
+    WorkOutline,
+    Event,
+    School,
+  } from "@material-ui/icons";
+
 
 export default function Sidebar() {
   return (
-    <div className='sidebar'>Sidebar</div>
+    <div className='sidebar'>
+        <div className='sidebarWrapper'>
+
+        </div>
+    </div>
   )
 }
